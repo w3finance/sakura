5fae7b954a9f2bc3b475f888a5d517509e3ff810
HiZhaoYun <hizhaoyun@gmail.com>
2020-02-22 19:16:11 +0800

    support change language

--- src/common/i18n.js
+++ src/common/i18n.js
@@ -6,11 +6,15 @@ i18n.init({
             translation: {
                 en: "English",
                 zh: "简体中文",
+                bt_cancel: "Cancel",
+                bt_ok: "OK",
                 tl_allAccounts: "All Accounts",
                 tl_addAccount: "Add Account",
                 tl_setting: "Setting",
                 tl_language: "Language",
                 tl_terms: "Terms of Use",
+
+                // All Accounts Page
                 a_noAccount: "No account yet!",
 
             }
@@ -19,12 +23,17 @@ i18n.init({
             translation: {
                 en: "English",
                 zh: "简体中文",
+                bt_cancel: "取消",
+                bt_ok: "确定",
                 tl_allAccounts: "全部账户",
                 tl_addAccount: "添加账户",
                 tl_setting: "设置",
                 tl_language: "多语言",
                 tl_terms: "用户协议",
+
+                // All Accounts Page
                 a_noAccount: "还没有账户!",
+
             }
         }
     },
--- src/pages/setting/setting.js
+++ src/pages/setting/setting.js
@@ -7,24 +7,20 @@ import {useTranslation} from "react-i18next";
 import {makeStyles} from "@material-ui/core/styles";
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 import {SettingsContext} from "../../context/setting";
-import Grid from '@material-ui/core/Grid';
 import {getAppVersion} from "../../common/call";
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+import Button from '@material-ui/core/Button';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
-const useStyles = makeStyles(theme => ({
-    version: {
-        color: 'rgba(16,16,16,.5)',
-        fontSize: 16,
-        width: '100%',
-        height: '50px',
-        textAlign: 'center'
-    },
-    footer: {
-        flexGrow: '1',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'flex-end',
-    }
-}));
+const options = [
+    'en',
+    'zh'
+];
 
 export default function Setting() {
     const history = useHistory();
@@ -33,6 +29,8 @@ export default function Setting() {
     const settings = useContext(SettingsContext);
     const classes = useStyles();
     const [version, setVersion] = useState();
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState(language);
 
     useEffect(() => {
         getAppVersion().then(res => {
@@ -40,23 +38,66 @@ export default function Setting() {
         }).catch(r => r);
     }, []);
 
-
     function back() {
         history.goBack();
     }
 
     function changeLng() {
-        if (i18n.language && i18n.language === 'zh') {
-            settings.toggleLanguage('en');
-            i18n.changeLanguage('en').then(r => r);
-        } else {
-            settings.toggleLanguage('zh');
-            i18n.changeLanguage('zh').then(r => r);
-        }
+        setValue(language);
+        setOpen(true);
     }
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleChange = event => {
+        setValue(event.target.value);
+    };
+
+    const handleOk = () => {
+        handleClose();
+        if (value) {
+            settings.toggleLanguage(value);
+            i18n.changeLanguage(value).then(r => r);
+        }
+    };
 
     return (
         <Wrapper>
+            <>
+                <Dialog open={open}
+                        onClose={handleClose}
+                        fullWidth={true}
+                        maxWidth={'xs'}
+                >
+                    <DialogTitle>{t('tl_language')}</DialogTitle>
+                    <DialogContent dividers>
+                        <RadioGroup
+                            aria-label="ringtone"
+                            name="ringtone"
+                            value={value}
+                            onChange={handleChange}
+                            color={'primary'}
+                        >
+                            {options.map(option => (
+                                <FormControlLabel value={option}
+                                                  key={option}
+                                                  control={<Radio color="primary"/>}
+                                                  label={t(`${option}`)}/>
+                            ))}
+                        </RadioGroup>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button autoFocus onClick={handleClose} color="primary">
+                            {t('bt_cancel')}
+                        </Button>
+                        <Button autoFocus onClick={handleOk} color="primary">
+                            {t('bt_ok')}
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </>
             <Header lfIcon bg title={t('tl_setting')} goBack={back}/>
             <Cell title={t('tl_language')}
                   subTitle={t(`${language}`)}
@@ -74,3 +115,19 @@ export default function Setting() {
         </Wrapper>
     )
 }
+
+const useStyles = makeStyles(theme => ({
+    version: {
+        color: 'rgba(16,16,16,.5)',
+        fontSize: 16,
+        width: '100%',
+        height: '50px',
+        textAlign: 'center'
+    },
+    footer: {
+        flexGrow: '1',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
+    }
+}));
